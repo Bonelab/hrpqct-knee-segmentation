4325b01ccab699284bb3da17749827accfca2a95
Nathan <njneeteson@gmail.com>
2022-09-30 22:06:50 -0600

    modify preprocess slurm scripts for new file structure

---
+++ slurm/preprocessing/preprocess.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+study="NORMXTII"
+for site in radius tibia
+do
+  for i in {0..11}
+  do
+    idx_start=$((100*i))
+    idx_end=$((100*(i+1)))
+    sbatch --export=IS="$idx_start",IE="$idx_end",STUDY="$study",SITE="$site" slurm/preprocessing/preprocess_2d.slurm
+    sleep 1
+    sbatch --export=IS="$idx_start",IE="$idx_end",STUDY="$study",SITE="$site" slurm/preprocessing/preprocess_3d.slurm
+    sleep 1
+  done
+done
+study="HIPFX"
+for site in radius tibia
+do
+  for i in {0..1}
+  do
+    idx_start=$((100*i))
+    idx_end=$((100*(i+1)))
+    sbatch --export=IS="$idx_start",IE="$idx_end",STUDY="$study",SITE="$site" slurm/preprocessing/preprocess_2d.slurm
+    sleep 1
+    sbatch --export=IS="$idx_start",IE="$idx_end",STUDY="$study",SITE="$site" slurm/preprocessing/preprocess_3d.slurm
+    sleep 1
+  done
+done
--- slurm/preprocessing/preprocess_2d.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in {0..26}
-do
-  idx_start=$((100*i))
-  idx_end=$((100*(i+1)))
-  sbatch --export=IS="$idx_start",IE="$idx_end" slurm/preprocessing/preprocess_2d.slurm
-  sleep 1
-done
