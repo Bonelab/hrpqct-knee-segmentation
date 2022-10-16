7abe7c6176f075ae60ea63f11f57e7722f78a7e5
Nathan <njneeteson@gmail.com>
2022-10-15 23:54:04 -0600

    slurm scripts are now a grid sweep

--- slurm/training/train_unet_2d_cv.sh
+++ slurm/training/train_unet_2d_cv.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-channels="32 64 128 256"
-dropout="0.1"
-learning_rate="0.001"
-sbatch \
---export=CHANNELS="$channels",DROPOUT="$dropout",LEARNING_RATE="$learning_rate" \
-slurm/training/train_unet_2d_cv.slurm
+for channels in "32 64 128 256" "32 64 128 256 512" "64 128 256 512" "64 128 256 512 1024"
+do
+  for dropout in "0.1" "0.2" "0.3" "0.4" "0.5"
+  do
+    for learning_rate in "0.0001" "0.001" "0.01" "0.1"
+    do
+      sbatch \
+      --export=CHANNELS="$channels",DROPOUT="$dropout",LEARNING_RATE="$learning_rate" \
+      slurm/training/train_unet_2d_cv.slurm
+    done
+  done
+done
+
+
