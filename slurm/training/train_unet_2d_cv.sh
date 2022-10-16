a26d7617e729304faaceb07b1b8d7242bebf90a7
Nathan <njneeteson@gmail.com>
2022-10-15 23:57:54 -0600

    reduce breadth of gri search

--- slurm/training/train_unet_2d_cv.sh
+++ slurm/training/train_unet_2d_cv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for channels in "32 64 128 256" "32 64 128 256 512" "64 128 256 512" "64 128 256 512 1024"
+for channels in "32 64 128 256 512" "64 128 256 512" "64 128 256 512 1024"
 do
-  for dropout in "0.1" "0.2" "0.3" "0.4" "0.5"
+  for dropout in "0.1" "0.3" "0.5"
   do
-    for learning_rate in "0.0001" "0.001" "0.01" "0.1"
+    for learning_rate in "0.001" "0.01" "0.1"
     do
       sbatch \
       --export=CHANNELS="$channels",DROPOUT="$dropout",LEARNING_RATE="$learning_rate" \
