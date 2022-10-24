b7cdeeef53b3e3da3ab3b49b4b6a3c7f9d80b15a
Nathan <njneeteson@gmail.com>
2022-10-24 17:46:28 -0600

    automatic batch size and learning rate

--- slurm/training/train_segan_3d_cv.sh
+++ slurm/training/train_segan_3d_cv.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-for channels in "64 128 256" "32 64 128 256" "64 128 256 512"
+for channels in "16 32 64 128" "32 64 128 256" "64 128 256 512" "64 128 256 512 1024"
 do
   for dropout in "0.1" "0.3" "0.5"
   do
-    for learning_rate in "0.001" "0.01"
-    do
-      sbatch \
-      --export=CHANNELS="$channels",DROPOUT="$dropout",LEARNING_RATE="$learning_rate" \
-      slurm/training/train_segan_3d_cv.slurm
-      sleep 1
-    done
+    sbatch \
+    --export=CHANNELS="$channels",DROPOUT="$dropout" slurm/training/train_segan_3d_cv.slurm
+    sleep 1
   done
 done
