af6784004ed3fc32213a2762b77fdd09759d7673
Nathan <njneeteson@gmail.com>
2022-10-25 10:53:22 -0600

    fix up 3d training submit scripts

---
+++ slurm/training/train_unet_3d_cv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for channels in "16 32 64 128" "32 64 128 256" "64 128 256 512"
+do
+  for dropout in "0.1" "0.3" "0.5"
+  do
+    sbatch \
+    --export=CHANNELS="$channels",DROPOUT="$dropout" slurm/training/train_unet_3d_cv.slurm
+    sleep 1
+  done
+done
