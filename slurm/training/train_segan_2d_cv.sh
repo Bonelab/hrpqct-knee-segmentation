c93c18a813d8bd0442399c5038e5b3ab0fbcffeb
Nathan <njneeteson@gmail.com>
2022-10-27 10:28:46 -0600

    fix up segan batch scripts

--- slurm/training/train_segan_2d_cv.sh
+++ slurm/training/train_segan_2d_cv.sh
@@ -3,8 +3,12 @@ for channels in "32 64 128 256" "64 128 256 512" "32 64 128 256 512"
 do
   for dropout in "0.1" "0.3" "0.5"
   do
-    sbatch \
-    --export=CHANNELS="$channels",DROPOUT="$dropout" slurm/training/train_segan_2d_cv.slurm
-    sleep 1
+    for learning_rate in "0.001" "0.0001"
+    do
+      sbatch \
+      --export=CHANNELS="$channels",DROPOUT="$dropout",LEARNING_RATE="$learning_rate" \
+      slurm/training/train_segan_2d_cv.slurm
+      sleep 1
+    done
   done
 done
