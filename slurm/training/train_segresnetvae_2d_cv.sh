c4f2370d35487f93497a9657549109a6718ecaca
Nathan <njneeteson@gmail.com>
2022-10-28 11:53:31 -0600

    segresnetvae batch scripts

---
+++ slurm/training/train_segresnetvae_2d_cv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for dropout in "0.1" "0.3" "0.5"
+do
+  for learning_rate in "0.001" "0.0001"
+  do
+    sbatch \
+    --export=DROPOUT="$dropout",LEARNING_RATE="$learning_rate" \
+    slurm/training/train_segresnetvae_2d_cv.slurm
+    sleep 1
+  done
+done
