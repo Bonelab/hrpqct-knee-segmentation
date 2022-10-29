21ef505e0a083526e0172a9ac46e3aebeef72649
Nathan <njneeteson@gmail.com>
2022-10-28 20:28:33 -0600

    unetpp training script

---
+++ slurm/training/train_unetpp_3d_cv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for channels in "16 16 32 64 32 16" "32 32 64 128 64 32"
+do
+  for dropout in "0.1" "0.3" "0.5"
+  do
+    for learning_rate in "0.001" "0.0001"
+    do
+      sbatch \
+      --export=CHANNELS="$channels",DROPOUT="$dropout",LEARNING_RATE="$learning_rate" \
+      slurm/training/train_unetpp_3d_cv.slurm
+      sleep 1
+    done
+  done
+done
