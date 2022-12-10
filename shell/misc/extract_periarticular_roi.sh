2a9bf104136d3aa754063c1a61c6e204546cd460
Nathan <njneeteson@gmail.com>
2022-12-09 18:22:43 -0700

    more memory for femurs

--- shell/misc/extract_periarticular_roi.sh
+++ shell/misc/extract_periarticular_roi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for visit_num in 1 2 3 4
 do
-  for bone in "femur" "tibia"
+  for bone in "femur" # "tibia"
   do
     sbatch --export=VISIT_NUM="$visit_num",BONE="$bone" slurm/misc/extract_periarticular_roi.slurm
     sleep 1
