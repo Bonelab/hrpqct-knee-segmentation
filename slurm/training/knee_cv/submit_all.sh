4ceeff9f876c9040328d727da41819b1a336cda4
Nathan <njneeteson@gmail.com>
2023-01-18 15:55:13 -0700

    add submit all script

---
+++ slurm/training/knee_cv/submit_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./shell/training/knee_cv/train_segan_3d_cv.sh
+./shell/training/knee_cv/train_segresnetvae_3d_cv.sh
+./shell/training/knee_cv/train_unet_3d_cv.sh
+./shell/training/knee_cv/train_unetpp_3d_cv.sh
+./shell/training/knee_cv/train_unetr_3d_cv.sh
