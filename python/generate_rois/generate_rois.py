94ce0e16acc27f26cecc14241cc2abfcdb1ef7e9
Nathan <njneeteson@gmail.com>
2023-09-06 13:38:27 -0600

    just inference and postproc

--- python/generate_rois/generate_rois.py
+++ python/generate_rois/generate_rois.py
@@ -436,6 +436,7 @@ def generate_rois(args: Namespace):
     message_s("Performing inference on image...", args.silent)
     model_mask = ensemble_model(image)
     message_s("Postprocessing model masks...", args.silent)
+    '''
     subchondral_bone_plate_mask, trabecular_bone_mask = postprocess_model_masks(
         (model_mask == args.model_subchondral_bone_plate_class).astype(int),
         (model_mask == args.model_trabecular_bone_class).astype(int),
@@ -462,10 +463,12 @@ def generate_rois(args: Namespace):
         args.minimum_subchondral_bone_plate_thickness,
         args.silent
     )
+    '''
     message_s("Writing model mask...", args.silent)
     model_mask_sitk = sitk.GetImageFromArray(model_mask)
     model_mask_sitk.CopyInformation(atlas_mask_sitk)
     sitk.WriteImage(sitk.Cast(model_mask_sitk, sitk.sitkInt32), model_mask_fn)
+    '''
     # create an all rois mask to start adding to
     all_rois_mask = sitk.Image(*model_mask_sitk.GetSize(), model_mask_sitk.GetPixelID())
     all_rois_mask = sitk.Cast(all_rois_mask, sitk.sitkInt32)
@@ -479,6 +482,7 @@ def generate_rois(args: Namespace):
 
     message_s("Writing all rois mask...", args.silent)
     sitk.WriteImage(all_rois_mask, allrois_mask_fn)
+    '''
 
 
 def create_parser() -> ArgumentParser:
