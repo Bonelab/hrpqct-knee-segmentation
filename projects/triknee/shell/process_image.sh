b603b9c37ba3b3bc68bdbb15fdf11979f81f771f
Nathan <njneeteson@gmail.com>
2023-09-21 08:56:38 -0600

    fix paths

--- projects/triknee/shell/process_image.sh
+++ projects/triknee/shell/process_image.sh
@@ -2,6 +2,6 @@
 IMAGE=$1
 BONE=$2
 LEFT=$3
-jid=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},LEFT=${LEFT} slurm/0_preinference.slurm | tr -dc "0-9")
-jid=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},LEFT=${LEFT} --dependency=afterany:${jid} slurm/1_inference.slurm | tr -dc "0-9")
-sbatch --export=IMAGE=${IMAGE},BONE=${BONE},LEFT=${LEFT} --dependency=afterany:${jid} slurm/2_postinference.slurm
+jid=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},LEFT=${LEFT} projects/triknee/slurm/0_preinference.slurm | tr -dc "0-9")
+jid=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},LEFT=${LEFT} --dependency=afterany:${jid} projects/triknee/slurm/1_inference.slurm | tr -dc "0-9")
+sbatch --export=IMAGE=${IMAGE},BONE=${BONE},LEFT=${LEFT} --dependency=afterany:${jid} projects/triknee/slurm/2_postinference.slurm
