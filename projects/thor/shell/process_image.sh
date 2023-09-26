ff3a82926cdd0991fe193c4e1ce8db34e6cb813b
Nathan <njneeteson@gmail.com>
2023-09-26 15:44:31 -0600

    fix

--- projects/thor/shell/process_image.sh
+++ projects/thor/shell/process_image.sh
@@ -4,30 +4,30 @@ then
   echo "Error: not enough arguments given."
   echo "Required arguments:"
   echo "--------------------"
-  echo "Argument 1: name of the AIM file to process, in /work/boyd_lab/data/TRIKNEE/aims"
+  echo "Argument 1: name of the AIM file to process, in /work/boyd_lab/data/THOR/aims"
   echo "Argument 2: which bone that image is, either \`tibia\` or \`femur\`"
   echo "Argument 3: which side that image is, either \`left\` or \`right\`"
   echo "--------------------"
-  echo "Example usage: ./projects/triknee/shell/process_image.sh TRIKNEE_01_TL_1 tibia left"
+  echo "Example usage: ./projects/thor/shell/process_image.sh THOR_1 tibia left"
   echo ""
   exit
 fi
 IMAGE=$1
 BONE=$2
 SIDE=$3
-JID_PRE=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} projects/triknee/slurm/0_preinference.slurm | tr -dc "0-9")
+JID_PRE=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} projects/thor/slurm/0_preinference.slurm | tr -dc "0-9")
 echo "Submitted job ${JID_PRE} to perform pre-inference processing."
 sleep 1
-JID_INF=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_PRE} projects/triknee/slurm/1_inference_segresnetvae.slurm | tr -dc "0-9")
+JID_INF=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_PRE} projects/thor/slurm/1_inference_segresnetvae.slurm | tr -dc "0-9")
 echo "Submitted job ${JID_INF} to perform inference processing. Will not execute until job ${JID_PRE} is complete."
 sleep 1
-JID_POST=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_INF} projects/triknee/slurm/2_postinference.slurm | tr -dc "0-9")
+JID_POST=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_INF} projects/thor/slurm/2_postinference.slurm | tr -dc "0-9")
 echo "Submitted job ${JID_POST} to perform post-inference processing. Will not execute until job ${JID_INF} is complete."
 sleep 1
 if [ ${BONE} = "femur" ]; then
   for ROI_CODE in 10 11 12 13 14 15 16 17;
   do
-    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_POST} projects/triknee/slurm/3_convert_to_aim.slurm | tr -dc "0-9")
+    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_POST} projects/thor/slurm/3_convert_to_aim.slurm | tr -dc "0-9")
     echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_POST} is complete."
     sleep 1
   done
@@ -35,7 +35,7 @@ fi
 if [ ${BONE} = "tibia" ]; then
   for ROI_CODE in 30 31 32 33 34 35 36 37;
   do
-    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_POST} projects/triknee/slurm/3_convert_to_aim.slurm | tr -dc "0-9")
+    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_POST} projects/thor/slurm/3_convert_to_aim.slurm | tr -dc "0-9")
     echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_POST} is complete."
     sleep 1
   done
