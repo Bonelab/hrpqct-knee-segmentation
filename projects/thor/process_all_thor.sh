3af278872795e9848b6a9e984907a4077cc90803
Nathan <njneeteson@gmail.com>
2023-09-26 15:36:37 -0600

    set up for thor

---
+++ projects/thor/process_all_thor.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./projects/triknee/shell/process_image.sh T1_THOR_THOR592L_F femur left; sleep 1
+./projects/triknee/shell/process_image.sh T1_THOR_THOR592L_T tibia left; sleep 1
---
+++ projects/thor/shell/visualize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+  echo "Error: not enough arguments given."
+  echo "Required arguments:"
+  echo "--------------------"
+  echo "Argument 1: name of the nifti file to visualize (with ROI masks), in /work/boyd_lab/data/TRIKNEE/niftis"
+  echo "--------------------"
+  echo "Example usage: ./projects/triknee/shell/visualize.sh TRIKNEE_01_TL_1"
+  echo ""
+  exit
+fi
+IMAGE=$1
+sbatch --export=IMAGE=${IMAGE} projects/triknee/slurm/4_visualize.slurm
