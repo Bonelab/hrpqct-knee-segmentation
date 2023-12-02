945396f5715ff59ef6f41168dca3054912cb120d
Nathan <njneeteson@gmail.com>
2023-12-02 01:04:34 -0700

    rework the processing order of ops

---
+++ projects/triknee/shell/process_baseline_image.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+if [ $# -lt 3 ]
+then
+  echo "Error: not enough arguments given."
+  echo "Required arguments:"
+  echo "--------------------"
+  echo "Argument 1: name of the AIM file to process, in /work/boyd_lab/data/TRIKNEE/aims"
+  echo "Argument 2: which bone that image is, either \`tibia\` or \`femur\`"
+  echo "Argument 3: which side that image is, either \`left\` or \`right\`"
+  echo "--------------------"
+  echo "Example usage: ./projects/triknee/shell/process_image.sh TRIKNEE_01_TL_1 tibia left"
+  echo ""
+  exit
+fi
+IMAGE=$1
+BONE=$2
+SIDE=$3
+JID_NII=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} projects/triknee/slurm/common/0_convert_to_nifti.slurm | tr -dc "0-9")
+echo "Submitted job ${JID_PRE} to convert AIM to nifti."
+sleep 0.1
+JID_INF=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_NII} projects/triknee/slurm/common/1_inference.slurm | tr -dc "0-9")
+echo "Submitted job ${JID_INF} to perform inference processing. Will not execute until job ${JID_NII} is complete."
+sleep 0.1
+JID_PP=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_INF} projects/triknee/slurm/common/2_postprocessing.slurm | tr -dc "0-9")
+echo "Submitted job ${JID_PP} to postprocess segmentation. Will not execute until job ${JID_INF} is complete."
+sleep 0.1
+JID_CTA=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_PP} projects/triknee/slurm/common/3_convert_segmentation_to_aims.slurm | tr -dc "0-9")
+echo "Submitted job ${JID_CTA} to convert the segmentation to AIMs. Will not execute until job ${JID_PP} is complete."
+sleep 0.1
+JID_AR=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_PP} projects/triknee/slurm/baseline/4_atlas_registration.slurm | tr -dc "0-9")
+echo "Submitted job ${JID_AR} to register the image to the atlas. Will not execute until job ${JID_PP} is complete."
+sleep 0.1
+JID_GR=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_AR} projects/triknee/slurm/baseline/5_generate_rois.slurm | tr -dc "0-9")
+echo "Submitted job ${JID_GR} to generate the ROIs. Will not execute until job ${JID_AR} is complete."
+sleep 0.1
+if [ ${BONE} = "femur" ]; then
+  for ROI_CODE in 10 11 12 13 14 15 16 17;
+  do
+    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/baseline/6_convert_to_aim.slurm | tr -dc "0-9")
+    echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
+    sleep 0.1
+  done
+fi
+if [ ${BONE} = "tibia" ]; then
+  for ROI_CODE in 30 31 32 33 34 35 36 37;
+  do
+    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/baseline/6_convert_to_aim.slurm | tr -dc "0-9")
+    echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
+    sleep 0.1
+  done
+fi
+sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE} --dependency=afterany:${JID_GR} projects/triknee/slurm/baseline/7_visualize.slurm
+echo "Submitted job to generate a visualization. Will not execute until job ${JID_GR} is complete."
+
+
---
+++ python/preprocessing/mask_image.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, Namespace, ArgumentTypeError
+import SimpleITK as sitk
+import numpy as np
+from skimage.morphology import binary_dilation
+
+from bonelab.util.time_stamp import message
+from bonelab.util.echo_arguments import echo_arguments
+from bonelab.cli.registration import check_inputs_exist, check_for_output_overwrite
+
+
+def expand_array_to_3d(array: np.ndarray, dim: int) -> np.ndarray:
+    if dim == 0:
+        return array[:, None, None]
+    elif dim == 1:
+        return array[None, :, None]
+    elif dim == 2:
+        return array[None, None, :]
+    else:
+        raise ValueError("`dim` must be 0, 1, or 2")
+
+
+def create_efficient_3d_binary_operation(func: callable) -> callable:
+    def efficient_3d_binary_operation(mask: np.ndarray, radius: int) -> np.ndarray:
+        for dim in [0, 1, 2]:
+            mask = func(mask, expand_array_to_3d(np.ones((2 * radius + 1,)), dim))
+        return mask
+    return efficient_3d_binary_operation
+
+
+def efficient_3d_dilation(mask: np.ndarray, radius: int) -> np.ndarray:
+    return create_efficient_3d_binary_operation(binary_dilation)(mask, radius)
+
+
+def message_s(m: str, s: bool):
+    """
+    Print a message if silent is False.
+
+    Parameters
+    ----------
+    m : str
+        The message to print.
+    s : bool
+        Whether or not to print the message.
+
+    Returns
+    -------
+    None
+    """
+    if not s:
+        message(m)
+
+
+def mask_image(args: Namespace) -> None:
+    echo_arguments("Mask image", vars(args))
+    check_inputs_exist([args.input, args.mask], args.silent)
+    check_for_output_overwrite(args.output, args.overwrite, args.silent)
+    message_s("Reading input image...", args.silent)
+    image, mask = sitk.ReadImage(args.input), sitk.ReadImage(args.mask)
+    image_array, mask_array = sitk.GetArrayFromImage(image), sitk.GetArrayFromImage(mask)
+    mask_array = mask_array != args.background_class
+    if args.dilate_amount > 0:
+        message_s("Dilating mask...", args.silent)
+        mask_array = efficient_3d_dilation(mask_array, args.dilate_amount)
+    message_s("Masking image...", args.silent)
+    image_array[~mask_array] = 0
+    message_s("Writing output image...", args.silent)
+    output = sitk.GetImageFromArray(image_array)
+    output.CopyInformation(image)
+    sitk.WriteImage(output, args.output)
+
+
+def create_parser() -> ArgumentParser:
+    parser = ArgumentParser(
+        description="This tool allows you to mask an image.",
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "input", type=str, help="The input image."
+    )
+    parser.add_argument(
+        "mask", type=str, help="The mask image."
+    )
+    parser.add_argument(
+        "output", type=str, help="The output image."
+    )
+    parser.add_argument(
+        "--dilate-amount", "-da", type=int, default=0, metavar="N",
+        help="The amount to dilate the mask by."
+    )
+    parser.add_argument(
+        "--background-class", "-bc", type=int, default=0, metavar="N",
+        help="The class to use for the background."
+    )
+    parser.add_argument(
+        "--overwrite", "-ow", action="store_true", help="Overwrite output file if it exists."
+    )
+    parser.add_argument(
+        "--silent", "-s", action="store_true", help="Silence all terminal output."
+    )
+    return parser
+
+
+def main() -> None:
+    args = create_parser().parse_args()
+    mask_image(args)
+
+
+if __name__ == "__main__":
+    main()
