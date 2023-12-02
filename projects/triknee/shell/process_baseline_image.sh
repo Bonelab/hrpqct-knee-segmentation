cf8826bd5fc864c8f417e4c1910a741dfc9af703
Nathan <njneeteson@gmail.com>
2023-12-02 01:07:15 -0700

    fix

--- projects/triknee/shell/process_baseline_image.sh
+++ projects/triknee/shell/process_baseline_image.sh
@@ -36,7 +36,7 @@ sleep 0.1
 if [ ${BONE} = "femur" ]; then
   for ROI_CODE in 10 11 12 13 14 15 16 17;
   do
-    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/baseline/6_convert_to_aim.slurm | tr -dc "0-9")
+    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/baseline/6_convert_roi_to_aim.slurm | tr -dc "0-9")
     echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
     sleep 0.1
   done
@@ -44,7 +44,7 @@ fi
 if [ ${BONE} = "tibia" ]; then
   for ROI_CODE in 30 31 32 33 34 35 36 37;
   do
-    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/baseline/6_convert_to_aim.slurm | tr -dc "0-9")
+    JID_ROI=$(sbatch --export=IMAGE=${IMAGE},BONE=${BONE},SIDE=${SIDE},ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/baseline/6_convert_roi_to_aim.slurm | tr -dc "0-9")
     echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
     sleep 0.1
   done
