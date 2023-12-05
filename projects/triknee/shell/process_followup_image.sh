8f77c4c12efbe98d7d3f5e35871045bd50834e40
Nathan <njneeteson@gmail.com>
2023-12-05 13:00:03 -0700

    start testing long reg

---
+++ projects/triknee/shell/process_followup_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -lt 3 ]
+then
+  echo "Error: not enough arguments given."
+  echo "Required arguments:"
+  echo "--------------------"
+  echo "Argument 1: LABEL of the AIM file to process, in /work/boyd_lab/data/TRIKNEE/aims"
+  echo "Argument 2: followup image number"
+  echo "Argument 3: which bone that image is, either \`tibia\` or \`femur\`"
+  echo "--------------------"
+  echo "Example usage: ./projects/triknee/shell/process_followup_image.sh TRIKNEE_01_TL 2 tibia"
+  echo ""
+  exit
+fi
+LABEL=$1
+FOLLOWUP=$2
+BONE=$2
+JID_REG=$(sbatch --export=LABEL=${LABEL},FOLLOWUP=${FOLLOWUP} projects/triknee/slurm/followup/4_longitudinal_registration.slurm | tr -dc "0-9")
+echo "Submitted job ${JID_REG} to register the followup image to the baseline image."
