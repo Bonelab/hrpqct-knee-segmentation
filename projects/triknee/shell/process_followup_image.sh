d4ea62f2b0207d9a667146204b4f4ece76c6aaa8
Nathan <njneeteson@gmail.com>
2023-12-10 21:49:06 -0700

    fixes

--- projects/triknee/shell/process_followup_image.sh
+++ projects/triknee/shell/process_followup_image.sh
@@ -22,12 +22,12 @@ BONE=$2
 if [ ${BONE} = "femur" ]; then
   for ROI_CODE in 10 11 12 13 14 15 16 17;
   do
-    JID_ROI=$(sbatch --export=IMAGE=${LABEL}_1,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
-    echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
-    sleep 0.1
-    JID_ROI=$(sbatch --export=IMAGE=${LABEL}_2,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
-    echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
-    sleep 0.1
+    #JID_ROI=$(sbatch --export=IMAGE=${LABEL}_1,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
+    #echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
+    #sleep 0.1
+    #JID_ROI=$(sbatch --export=IMAGE=${LABEL}_2,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
+    #echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
+    #sleep 0.1
     JID_ROI=$(sbatch --export=IMAGE=${LABEL}_3,ROI_CODE=${ROI_CODE}  projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
     echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
     sleep 0.1
@@ -36,12 +36,12 @@ fi
 if [ ${BONE} = "tibia" ]; then
   for ROI_CODE in 30 31 32 33 34 35 36 37;
   do
-    JID_ROI=$(sbatch --export=IMAGE=${LABEL}_1,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
-    echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
-    sleep 0.1
-    JID_ROI=$(sbatch --export=IMAGE=${LABEL}_2,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
-    echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
-    sleep 0.1
+    #JID_ROI=$(sbatch --export=IMAGE=${LABEL}_1,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
+    #echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
+    #sleep 0.1
+    #JID_ROI=$(sbatch --export=IMAGE=${LABEL}_2,ROI_CODE=${ROI_CODE} --dependency=afterany:${JID_GR} projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
+    #echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
+    #sleep 0.1
     JID_ROI=$(sbatch --export=IMAGE=${LABEL}_3,ROI_CODE=${ROI_CODE}  projects/triknee/slurm/followup/6_convert_roi_to_aim.slurm | tr -dc "0-9")
     echo "Submitted job ${JID_ROI} to convert the ROI${ROI_CODE} mask to AIM format. Will not execute until job ${JID_GR} is complete."
     sleep 0.1
