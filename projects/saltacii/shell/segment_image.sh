35107d919048e5540c5f74cbcbf9e8de4d90a8db
Nathan Neeteson <njneeteson@gmail.com>
2024-02-06 11:28:00 -0700

    also convert tunnel masks to aims

--- projects/saltacii/shell/segment_image.sh
+++ projects/saltacii/shell/segment_image.sh
@@ -22,11 +22,13 @@ sleep 0.1
 if [ ${SURGERY} = "yes" ]; then
   JID_PP=$(sbatch --export=IMAGE=${IMAGE} --dependency=afterany:${JID_INF} projects/saltacii/slurm/segmentation/2b_postprocessing_tunnel.slurm | tr -dc "0-9")
   echo "Submitted job ${JID_PP} to postprocess segmentation with tunnel detection. Will not execute until job ${JID_INF} is complete."
+  sbatch --export=IMAGE=${IMAGE} --dependency=afterany:${JID_PP} projects/saltacii/slurm/segmentation/3b_convert_segmentation_to_aims_tunnel.slurm
+  echo "Submitted job to convert to AIMs. Will not execute until job ${JID_PP} is complete."
 else
   JID_PP=$(sbatch --export=IMAGE=${IMAGE} --dependency=afterany:${JID_INF} projects/saltacii/slurm/segmentation/2a_postprocessing.slurm | tr -dc "0-9")
   echo "Submitted job ${JID_PP} to postprocess segmentation. Will not execute until job ${JID_INF} is complete."
+  sbatch --export=IMAGE=${IMAGE} --dependency=afterany:${JID_PP} projects/saltacii/slurm/segmentation/3a_convert_segmentation_to_aims.slurm
+  echo "Submitted job to convert to AIMs. Will not execute until job ${JID_PP} is complete."
 fi
-sbatch --export=IMAGE=${IMAGE} --dependency=afterany:${JID_PP} projects/saltacii/slurm/segmentation/3_convert_segmentation_to_aims.slurm
-echo "Submitted job to convert to AIMs. Will not execute until job ${JID_PP} is complete."
 sbatch --export=IMAGE=${IMAGE} --dependency=afterany:${JID_PP} projects/saltacii/slurm/segmentation/4_visualize.slurm
 echo "Submitted job to generate a visualization. Will not execute until job ${JID_PP} is complete."
