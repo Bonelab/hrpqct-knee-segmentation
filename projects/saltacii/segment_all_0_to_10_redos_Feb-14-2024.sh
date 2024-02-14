adabd99d6ac5756ae3f0d9f1e0f869c5ade3c0c1
Nathan Neeteson <njneeteson@gmail.com>
2024-02-14 13:26:15 -0700

    add redo script for segmenting

---
+++ projects/saltacii/segment_all_0_to_10_redos_Feb-14-2024.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# this script exists because some images needed to be recropped and reprocessed after they were checked
+# sltcii0002_fl_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0002_FL_V0S no; sleep 0.1
+# sltcii0002_fr_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0002_FR_V03 yes; sleep 0.1
+# sltcii0002_fr_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0002_FR_V0S yes; sleep 0.1
+# sltcii0003_fl_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0003_FL_V03 yes; sleep 0.1
+# sltcii0003_fl_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0003_FL_V0S yes; sleep 0.1
+# sltcii0003_fr_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0003_FR_V03 no; sleep 0.1
+# sltcii0003_fr_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0003_FR_V0S no; sleep 0.1
+# sltcii0003_tr_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0003_TR_V03 no; sleep 0.1
+# sltcii0004_fl_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0004_FL_V03 yes; sleep 0.1
+# sltcii0004_fl_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0004_FL_V0S yes; sleep 0.1
+# sltcii0004_fr_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0004_FR_V03 no; sleep 0.1
+# sltcii0004_fr_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0004_FR_V0S no; sleep 0.1
+# sltcii0005_fl_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0005_FL_V03 yes; sleep 0.1
+# sltcii0005_fl_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0005_FL_V0S yes; sleep 0.1
+# sltcii0005_fr_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0005_FR_V03 no; sleep 0.1
+# sltcii0005_fr_v0s
+./projects/saltacii/shell/segment_image.sh SLTCII0005_FR_V0S no; sleep 0.1
+# sltcii0008_fl_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0008_FL_V03 no; sleep 0.1
+# sltcii0008_fr_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0008_FR_V03 no; sleep 0.1
+# sltcii0009_fl_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0009_FL_V03 no; sleep 0.1
+# sltcii0009_fr_v03
+./projects/saltacii/shell/segment_image.sh SLTCII0009_FR_V03 yes; sleep 0.1
