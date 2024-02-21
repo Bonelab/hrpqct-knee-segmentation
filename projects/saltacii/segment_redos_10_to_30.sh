8066b8f5b74804580929b2b77f649dd2542129d7
Nathan Neeteson <njneeteson@gmail.com>
2024-02-20 21:35:54 -0700

    add script

---
+++ projects/saltacii/segment_redos_10_to_30.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# this script exists because some images had to be recropped and resegmented
+./projects/saltacii/shell/segment_image.sh SLTCII0012_FL_V03 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0012_FL_V0S no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0012_FR_V03 yes; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0012_FR_V0S yes; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0013_FR_V03 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0014_FL_V03 yes; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0014_FR_V03 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0015_FL_V02 yes; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0016_FL_V02 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0016_FL_V03 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0016_FR_V03 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0017_FL_V03 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0018_FR_V03 no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0018_FR_V0S no; sleep 0.1
+./projects/saltacii/shell/segment_image.sh SLTCII0023_FR_V0S no; sleep 0.1 
