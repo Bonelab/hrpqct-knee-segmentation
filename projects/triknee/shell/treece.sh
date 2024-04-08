0f3462a50dfc57246f0df72ca25e1938ad371c92
Nathan Neeteson <njneeteson@gmail.com>
2024-04-07 20:30:01 -0600

    small edit

--- projects/triknee/shell/treece.sh
+++ projects/triknee/shell/treece.sh
@@ -6,7 +6,7 @@ fi
 IMAGE=$1
 COMPARTMENT=$2
 REG=$3
-ROI_CODE=$5
+ROI_CODE=$4
 sbatch \
 --export=IMAGE=${IMAGE},COMPARTMENT=${COMPARTMENT},REG=${REG},ROI_CODE=${ROI_CODE} \
 projects/triknee/slurm/treece/get_thickness.slurm
