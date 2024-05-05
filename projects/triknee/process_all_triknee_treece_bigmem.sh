03ec957b32c6dc6e0b709585419a11af29fc440c
Nathan Neeteson <njneeteson@gmail.com>
2024-05-04 21:29:57 -0600

    commit

---
+++ projects/triknee/process_all_triknee_treece_bigmem.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./projects/triknee/shell/treece.sh triknee_01_tl_1 medial noreg 37
+./projects/triknee/shell/treece.sh triknee_01_tl_3 medial noreg 37
+./projects/triknee/shell/treece.sh triknee_03_tl_1 medial noreg 37
+./projects/triknee/shell/treece.sh triknee_22_tr_1 lateral reg 36
+./projects/triknee/shell/treece.sh triknee_25_tl_3 lateral noreg 36
+./projects/triknee/shell/treece.sh triknee_30_tl_2 lateral reg 36
+./projects/triknee/shell/treece.sh triknee_30_tl_3 medial noreg 37
